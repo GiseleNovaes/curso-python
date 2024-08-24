c135d6d940fc856ed328889733b1ea45d12f1fed
Gisele Novaes <gisellemnovaes@hotmail.com>
2024-08-23 23:39:39 -0300

    Iniciando o Curso

---
+++ Aula1_meu_primeiro_programa.py
@@ -0,0 +1,6 @@
+"""
+Observe que na linguagem Python, usamos a tabulação, ou seja, a indentação,
+para separar os blocos de código. É importante se atentar à ordem das linhas
+para escrever os códigos e evitar erros na execução.
+"""
+print("Olá Mundo!")
---
+++ Aula2_VAriaveis_e_tipo_de_dados.py
@@ -0,0 +1,14 @@
+#Aqui um texto que não muda com uma frase em cada linha.
+"""print("Conheço um homem chamado José,")
+print("Ele tem 70 anos")
+print("Ele gosta de se chamr José")
+print("Mas não gosta de ter 70 anos.")"""
+
+#Aqui um texto que pode ser mudado várias vezes algumas partes usando Variáveis.
+nome_personagem = "Otávio" #Variável do tipo String (Nome, palavra)
+idade_personagem = "70"    #Variável do tipo int (inteiro)
+
+print("Conheço um homem chamado " + nome_personagem + ",")
+print("Ele tem " + idade_personagem + " anos")
+print("Ele gosta de se chamar " + nome_personagem)
+print("Mas não gosta de ter " + idade_personagem + " anos.")
